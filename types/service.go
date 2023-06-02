94baa14a2e8c1dcca31b11d1ad9ca92349fb74bd
efford.wang <efford.wang@applovin.com>
2023-06-02 16:58:00 +0800

    add payload for service and user

--- types/service.go
+++ types/service.go
@@ -1,1 +1,8 @@
 package types
+
+type ServicePayload struct {
+	Name                   string `json:"name"`
+	Description            string `json:"description"`
+	AutoResolve            bool   `json:"auto_resolve_timeout"`
+	AcknowledgementTimeout int    `json:"acknowledgement_timeout"`
+}
--- types/user.go
+++ types/user.go
@@ -1,1 +1,11 @@
 package types
+
+type UserPayload struct {
+	Type           string          `json:"type"`
+	Name           string          `json:"name"`
+	Email          string          `json:"email"`
+	Timezone       string          `json:"timezone"`
+	Role           string          `json:"role"`
+	ContactMethods []ContactMethod `json:"contact_methods,omitempty"`
+	TeamIDs        []string        `json:"team_ids,omitempty"`
+}
